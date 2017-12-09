fbaf491513083820edda2f37727ead8061137435
Paulo Castro <p.oliveira.castro@gmail.com>
2017-12-09 18:58:15 -0200

    add probabilities to /food response

--- backend/server.py
+++ backend/server.py
@@ -9,24 +9,27 @@ from keras.applications.resnet50 import preprocess_input
 from keras.preprocessing.image import img_to_array, load_img
 from flask import Flask, request
 
-app = Flask(__name__)
-cors = CORS(app)
-
-UPLOAD_FOLDER = 'images/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
 def keras_load_model():
     model = load_model('../weights/model-v1.h5')
     return model
 
-def get_label(output):
-    output = np.argmax(output)
-    return classes[str(output)]
+def get_label(probabilities):
+    probabilities = probabilities[0].astype('float64')
+    top_outputs = probabilities.argsort()[-5:][::-1]
+    top_probs = [probabilities[output] for output in top_outputs]
+    result = [(prob, classes[str(output)]) for prob, output in zip(top_probs, top_outputs)]
+    return result
 
+app = Flask(__name__)
+cors = CORS(app)
+UPLOAD_FOLDER = 'images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 model = keras_load_model()
 model._make_predict_function()
 classes = json.load(open("../data/classes.json"))
+wine_table = pd.read_csv("../data/wine_table.csv")
+food_wine = json.load(open('../data/food_wine.json'))
+wine_table.index = wine_table.name
 
 @app.route('/food', methods=['POST'])
 def food_kind():
@@ -37,14 +40,11 @@ def food_kind():
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
     prediction = model.predict(img_array)
-    food = get_label(prediction)
-    return food, 200
+    foods = get_label(prediction)
+    return json.dumps(foods), 200
 
 @app.route('/wine/<food>', methods=['GET'])
 def wine_that_matched(food):
-    food_wine = json.load(open('../data/food_wine.json'))
-    wine_table = pd.read_csv("../data/wine_table.csv")
-    wine_table.index = wine_table.name
     try:
         wine = food_wine[food]
     except KeyError:
